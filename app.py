41fc70d37c3db2c862571aa0efb5c7246f096448
Fatma___ <88388985+FatMAnsour@users.noreply.github.com>
2024-04-29 07:50:20 -0700

    getState editted

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ eye_tracker = EyeTracker()
 # Global variables for video capture and processing
 frame_processed = None
 frame_available = False
+image_received = False  # Flag to track if an image has been received
 
 def process_video(frame):
     global frame_processed, frame_available
@@ -24,22 +25,32 @@ def process_video(frame):
 
 @app.route('/start_video', methods=['POST'])
 def start_video():
-    global frame_processed, frame_available
-    frame_data = request.data  # Receive frame data from the Flutter mobile app
-    frame_array = np.frombuffer(frame_data, dtype=np.uint8)  # Convert data to numpy array
-    frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)  # Decode frame
+    global frame_processed, frame_available, image_received
+    # Receive frame data from the Flutter mobile app
+    frame_data = request.data  
+    # Convert data to numpy array
+    frame_array = np.frombuffer(frame_data, dtype=np.uint8)  
+    # Decode frame
+    frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)  
+    # Start processing video frame in a separate thread
     thread = threading.Thread(target=process_video, args=(frame,))
     thread.daemon = True
     thread.start()
+    image_received = True  # Set the flag to indicate image received
     return "Video capture started"
 
 @app.route('/get_state', methods=['GET'])
 def get_state():
-    global frame_available
-    if frame_available:
-        return jsonify({'state': eye_tracker.get_current_state()}), 200
+    global frame_available, image_received
+    if image_received:
+        if frame_available:
+            response_data = {'state': eye_tracker.get_current_state()}
+            image_received = False  # Reset the flag
+            return jsonify(response_data), 200
+        else:
+            return jsonify({'error': 'No processed frame available'}), 404
     else:
-        return jsonify({'error': 'No processed frame available'}), 404
+        return jsonify({'error': 'No image received yet'}), 404
 
 @app.route('/stop_video', methods=['POST'])
 def stop_video():
@@ -48,5 +59,6 @@ def stop_video():
     return "Video capture stopped"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('HOST_PORT', 10000))  # Default to 5000 if not set
+    port = int(os.environ.get('HOST_PORT', 10000))  
+    # Default to 5000 if not set
     app.run(host='0.0.0.0', port=port)  # Listen on all interfaces (0.0.0.0)
