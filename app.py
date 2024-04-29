b41d42caf331c258576d19d465e3187ab78229f6
Fatma___ <88388985+FatMAnsour@users.noreply.github.com>
2024-04-29 05:35:11 -0700

    frame changed

--- app.py
+++ app.py
@@ -1,49 +1,36 @@
-from flask import Flask, request, Response,jsonify
+from flask import Flask, request, Response, jsonify
 import cv2
 import numpy as np
 import threading
 import time
-from eye_tracking import EyeTracker
+from eye_tracking import EyeTracker  # Assuming you have the EyeTracker class implemented
 import os
-
 
 app = Flask(__name__)
 eye_tracker = EyeTracker()
 
 # Global variables for video capture and processing
-video_capture = None
 frame_processed = None
 frame_available = False
 
-def process_video():
-    global video_capture, frame_processed, frame_available
-    while True:
-        ret, frame = video_capture.read()
-        if ret:
-            processed_frame = eye_tracker.process_frames(frame)
-            frame_processed = processed_frame
-            frame_available = True
-        else:
-            frame_available = False
-            break
+def process_video(frame):
+    global frame_processed, frame_available
+    if frame is not None:
+        processed_frame = eye_tracker.process_frames(frame)
+        frame_processed = processed_frame
+        frame_available = True
+    else:
+        frame_available = False
 
-def start_video_capture():
-    global video_capture
-    video_capture = cv2.VideoCapture(0)
-    if not video_capture.isOpened():
-        print("Error: Unable to open video capture device")
-        return
-    thread = threading.Thread(target=process_video)
-    thread.daemon = True
-    thread.start()
-    
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-    
 @app.route('/start_video', methods=['POST'])
 def start_video():
-    start_video_capture()
+    global frame_processed, frame_available
+    frame_data = request.data  # Receive frame data from the Flutter mobile app
+    frame_array = np.frombuffer(frame_data, dtype=np.uint8)  # Convert data to numpy array
+    frame = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)  # Decode frame
+    thread = threading.Thread(target=process_video, args=(frame,))
+    thread.daemon = True
+    thread.start()
     return "Video capture started"
 
 @app.route('/get_state', methods=['GET'])
@@ -56,12 +43,10 @@ def get_state():
 
 @app.route('/stop_video', methods=['POST'])
 def stop_video():
-    global video_capture
-    if video_capture is not None:
-        video_capture.release()
-        video_capture = None
+    global frame_available
+    frame_available = False
     return "Video capture stopped"
 
 if __name__ == '__main__':
-  port = int(os.environ.get('HOST_PORT', 10000))  # Default to 5000 if not set
-  app.run(host='0.0.0.0', port=port)# Listen on all interfaces (0.0.0.0)
+    port = int(os.environ.get('HOST_PORT', 10000))  # Default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)  # Listen on all interfaces (0.0.0.0)
